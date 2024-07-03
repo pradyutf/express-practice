40f999b618c47d7700063721ab332e4ec4ab8b77
pradyutf <pradyutfogla@gmail.com>
2024-07-03 10:08:10 +0530

    post test

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const { json } = require('express/lib/response');
 const app = express();
+
+app.use(express.json());
 
 let courses = [
     { id: 1, name: 'course1' },
@@ -14,6 +17,20 @@ app.get('/courses', (req, res) => {
 app.get('/', (req, res) => {
     res.send('CHAL RHA HU BHAI');
 });
+
+app.post('/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.json(course);
+
+    // console.log(req.body);
+    // res.send('received');
+});
+
+
 
 
 app.listen(3000, () => console.log('Listening on port 3000...'));
